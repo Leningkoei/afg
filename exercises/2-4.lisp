e86f90ed56b2c267fd54bd8a7c4fc721ec2b0051
Leningkoei <zhanglening2001@gmail.com>
2022-09-01 00:03:35 +0900

    coupler

---
+++ exercises/2-4.lisp
@@ -0,0 +1,59 @@
+;;;; 2-4
+;;;; 2022/08/31
+
+(load "../algorithms/cross.lisp")
+(load "../algorithms/receive.lisp")
+(load "../algorithms/sect.lisp")
+
+;;; 1.
+
+(load "../algorithms/pipe.lisp")
+
+(defun pipe-3 (eariler middler later)
+  (lambda (&rest arguments)
+    (receive (&rest intermediates)
+        (apply (pipe eariler middler) arguments)
+      (apply later intermediates))))
+
+
+;;; 2.
+
+(defun compose-3 (inner border outer)
+  (lambda (&rest arguments)
+    (receive (&rest border)
+        (receive (&rest inner)
+            (apply inner arguments) ; <- this form can't read `inner` which is outside
+          (apply border inner))
+      (apply outer border))))
+
+
+;;; 3.
+
+(load "../algorithms/delist.lisp")
+
+(defun sum-of-reciprocals* (&rest arguments)
+  (apply #'+ (map 'list (sect #'/ 1 <>) arguments)))
+
+(defun sum-of-reciprocals (&rest arguments)
+  (apply (pipe-3 #'map #'delist #'+) (list 'list (sect #'/ 1 <>) arguments)))
+
+
+;;; 4.
+
+(load #p"../algorithms/dispatcher.lisp")
+
+(defun harmonic-mean (a b)
+  (let ((c (* 2 a b))
+        (d (+ a b)))
+    (/ c d)))
+
+(defun harmonic-mean (a b)
+  (/ (* 2 a b) (+ a b)))
+
+(defun harmonic-mean (a b)
+  (apply
+   (pipe (dispatcher (sect #'* 2 <> <>) (sect #'+ <> <>)) #'/)
+   (list a b)))
+  ;; (receive (&rest arguments)
+  ;;     (apply (dispatcher (sect #'* 2 <> <>) (sect #'+ <> <>)) (list a b))
+  ;;   (apply #'/ arguments)))
