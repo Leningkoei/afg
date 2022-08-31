6407032654af8725c1a356d699cecd4523108a9d
Leningkoei <zhanglening2001@gmail.com>
2022-08-31 18:11:03 +0900

    fix name of arguments

--- algorithms/sect.lisp
+++ algorithms/sect.lisp
@@ -9,18 +9,19 @@
 ;;   (expt 2 x))
 
 (defmacro sect (function &rest arguments)
-  (labels
-      ((find-and-change (rest-argument index)
-         (cond
-           ((null rest-argument) nil)
-           ((equal '<> (car rest-argument))
-            (cons
-             `(nth ,index arguments)
-             (find-and-change (cdr rest-argument) (+ index 1))))
-           ('t
-            (cons
-             (car rest-argument)
-             (find-and-change (cdr rest-argument) index))))))
-    (let ((arguments-kai (find-and-change arguments 0)))
-      `(lambda (&rest arguments)
-         (,function ,@arguments-kai)))))
+  (let ((best-name-for-arguments (gensym)))
+    (labels
+        ((find-and-change (rest-argument index)
+           (cond
+             ((null rest-argument) nil)
+             ((equal '<> (car rest-argument))
+              (cons
+               `(nth ,index ,best-name-for-arguments)
+               (find-and-change (cdr rest-argument) (+ index 1))))
+             ('t
+              (cons
+               (car rest-argument)
+               (find-and-change (cdr rest-argument) index))))))
+      (let ((arguments-kai (find-and-change arguments 0)))
+        `(lambda (&rest ,best-name-for-arguments)
+           (,function ,@arguments-kai))))))
